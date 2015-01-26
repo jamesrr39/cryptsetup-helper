5ce5f513ff9d91d3c57392ede7bcc90d9d18d4e6
James Ralphs <jr.ralphs@gmail.com>
2015-01-26 19:38:22 +0100

    scope of flags expanded

--- open/open.go
+++ open/open.go
@@ -7,17 +7,22 @@ import (
 	"os/exec"
 )
 
-func main() {
-	var containerName string
-	var containerFilePath string
-	var mountPath string
-	var deviceFolderPath string
+var containerName string
+var containerFilePath string
+var mountPath string
+var deviceFolderPath string
+var verbose bool
+
+func init() {
 	flag.StringVar(&containerFilePath, "fp", "", "filepath to the device to be opened")
 	flag.StringVar(&containerName, "n", "", "name of the device to be opened")
 	flag.StringVar(&mountPath, "mp", "", "path the device should be mounted at")
 	flag.StringVar(&deviceFolderPath, "dfp", "/dev/mapper/", "folder the device file should be created in, including the path separator ")
-
+	flag.BoolVar(&verbose, "v", false, "verbose output")
 	flag.Parse()
+}
+
+func main() {
 
 	OpenDMCryptContainer(containerFilePath, containerName)
 	devicePath := deviceFolderPath + containerName
@@ -37,10 +42,13 @@ func OpenDMCryptContainer(path string, containerName string) {
 
 func MountDevice(devicePath string, mountPath string) {
 	fmt.Println("Attempting to mount " + devicePath)
-	_, mountError := exec.Command("mount", devicePath, mountPath).Output()
-	if mountError != nil {
-		fmt.Println("Error mounting the device")
-	} else {
-		fmt.Println("Device mounted successfully at " + mountPath)
+	cmd := exec.Command("mount", devicePath, mountPath)
+	cmd.Stdout = os.Stdout
+	cmd.Stdin = os.Stdout
+	cmd.Stderr = os.Stderr
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error mounting " + devicePath + " at " + mountPath)
 	}
+
 }
