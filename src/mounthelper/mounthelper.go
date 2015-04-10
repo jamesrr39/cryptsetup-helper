44b21ef992b5a2006c5e25e7eba3086627d3ad82
James Ralphs <jr.ralphs@gmail.com>
2015-04-10 23:22:36 +0200

    create directory to mount in if necessary

--- src/mounthelper/mounthelper.go
+++ src/mounthelper/mounthelper.go
@@ -2,15 +2,24 @@ package mounthelper
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"os/exec"
 )
 
 func MountDevice(devicePath string, mountPath string) {
+	_, statErr := os.Stat(mountPath)
+	if os.IsNotExist(statErr) {
+		fmt.Println("Creating a new directory to mount the folder in at " + mountPath)
+		mkdirErr := os.Mkdir(mountPath, 0700)
+		if mkdirErr != nil {
+			log.Fatal("Couldn't create directory at " + mountPath)
+		}
+	}
 	fmt.Println("Attempting to mount " + devicePath)
 	cmd := exec.Command("mount", devicePath, mountPath)
 	cmd.Stdout = os.Stdout
-	cmd.Stdin = os.Stdout
+	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	err := cmd.Run()
 	if err != nil {
